7bafdde94abb94f549c061c50f34277c560858dc
seedevk8s <seedevk8s@gmail.com>
2024-12-23 17:52:08 +0900

    컨트롤러 메서드 추가 및 템플릿 파일 추가

--- src/main/java/com/intro/introspring/controller/IntroController.java
+++ src/main/java/com/intro/introspring/controller/IntroController.java
@@ -3,6 +3,7 @@ package com.intro.introspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IntroController {
@@ -11,5 +12,11 @@ public class IntroController {
     public String intro(Model model) {
         model.addAttribute("data", "Intro Spring!!");
         return "intro";
+    }
+
+    @GetMapping("intro-mvc")
+    public String introMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
