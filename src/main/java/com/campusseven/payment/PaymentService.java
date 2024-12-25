9e588fa550454a841e27a8d9b938cb4f0de57673
seedevk8s <seedevk8s@gmail.com>
2024-12-25 17:53:22 +0900

    환율등 정보 읽어오기 기능 추가

--- src/main/java/com/campusseven/payment/PaymentService.java
+++ src/main/java/com/campusseven/payment/PaymentService.java
@@ -1,18 +1,33 @@
 package com.campusseven.payment;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 public class PaymentService {
-    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
-        // 환율 가져오기
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
+        // 환율 가져오기  https://open.er-api.com/v6/latest/USD
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = br.lines().collect(Collectors.joining());
+        br.close();
+
+        System.out.println(response);
         // 금액 계산
         // 유효 시간 계산
         return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO,
                 LocalDateTime.now());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         PaymentService paymentService = new PaymentService();
         Payment payment = paymentService.prepare(100L,"USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
