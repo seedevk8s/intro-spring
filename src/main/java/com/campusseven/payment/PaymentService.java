b3339284694d39b489af64d9f0519e63fb48ac02
seedevk8s <seedevk8s@gmail.com>
2024-12-25 22:17:47 +0900

    has a 관계 객체 초기화 방법 리펙토링

--- src/main/java/com/campusseven/payment/PaymentService.java
+++ src/main/java/com/campusseven/payment/PaymentService.java
@@ -5,9 +5,17 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class PaymentService {
+    //private final WebApiExRateProvider exRateProvider;
+    private final SimpleExRateProvider exRateProvider;
+
+    public PaymentService() {
+        //this.exRateProvider = new WebApiExRateProvider();
+        this.exRateProvider = new SimpleExRateProvider();
+    }
+
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        WebApiExRateProvider exRateProvider = new WebApiExRateProvider();
-        BigDecimal exRate = exRateProvider.getWebExRate(currency);
+        //BigDecimal exRate = exRateProvider.getWebExRate(currency);
+        BigDecimal exRate = exRateProvider.getExRate(currency);
         // 금액 계산
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
         // 유효 시간 계산
