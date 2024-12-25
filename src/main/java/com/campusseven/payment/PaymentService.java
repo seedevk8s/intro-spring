94bedfc7bc9db8dbf691bd55b3457558023720da
seedevk8s <seedevk8s@gmail.com>
2024-12-25 20:23:40 +0900

    관심사를 두개로 분리함

--- src/main/java/com/campusseven/payment/PaymentService.java
+++ src/main/java/com/campusseven/payment/PaymentService.java
@@ -15,6 +15,17 @@ import java.util.stream.Collectors;
 
 public class PaymentService {
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
+        BigDecimal exRate = getExRate(currency);
+
+        // 금액 계산
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+        // 유효 시간 계산
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
+
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+    }
+
+    private BigDecimal getExRate(String currency) throws IOException {
         // 환율 가져오기  https://open.er-api.com/v6/latest/USD
         URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -28,13 +39,7 @@ public class PaymentService {
         //System.out.println(data);
         BigDecimal exRate = data.rates().get("KRW");
         //System.out.println(exRate);
-
-        // 금액 계산
-        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
-        // 유효 시간 계산
-        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
-
-        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+        return exRate;
     }
 
     public static void main(String[] args) throws IOException {
