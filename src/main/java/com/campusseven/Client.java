9201df8b284eaf89d461e546ff0b2acaae9c16ab
seedevk8s <seedevk8s@gmail.com>
2024-12-26 01:49:01 +0900

    싱글톤 객체로 만들어지는 확인하기

--- src/main/java/com/campusseven/Client.java
+++ src/main/java/com/campusseven/Client.java
@@ -11,6 +11,10 @@ public class Client {
     public static void main(String[] args) throws IOException {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
+        PaymentService paymentService2 = beanFactory.getBean(PaymentService.class);
+
+        System.out.println(paymentService); System.out.println(paymentService2);
+        System.out.println(paymentService == paymentService2);
 
         Payment payment = paymentService.prepare(100L,"USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
