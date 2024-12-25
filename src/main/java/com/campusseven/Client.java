b8277cdfd5e736b125528ab61dde6aef3a95bf2e
seedevk8s <seedevk8s@gmail.com>
2024-12-25 23:15:16 +0900

    client에 관계 설정 책임을 분리함

--- src/main/java/com/campusseven/Client.java
+++ src/main/java/com/campusseven/Client.java
@@ -3,14 +3,14 @@ package com.campusseven;
 import com.campusseven.payment.Payment;
 import com.campusseven.payment.PaymentService;
 import com.campusseven.payment.SimpleExRateProvider;
+import com.campusseven.payment.WebApiExRateProvider;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        //PaymentService paymentService = new WebApiExRatePaymentService();
-        PaymentService paymentService = new PaymentService();
+        PaymentService paymentService = new PaymentService(new SimpleExRateProvider());
         Payment payment = paymentService.prepare(100L,"USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
--- src/main/java/com/campusseven/payment/PaymentService.java
+++ src/main/java/com/campusseven/payment/PaymentService.java
@@ -5,24 +5,18 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class PaymentService {
-    //private final WebApiExRateProvider exRateProvider;
-   // private final SimpleExRateProvider exRateProvider;
     private final ExRateProvider exRateProvider;
 
-    public PaymentService() {
-        this.exRateProvider = new WebApiExRateProvider();
-        //this.exRateProvider = new SimpleExRateProvider();
+    public PaymentService(ExRateProvider exRateProvider) {
+        this.exRateProvider = exRateProvider;
     }
 
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        //BigDecimal exRate = exRateProvider.getWebExRate(currency);
         BigDecimal exRate = exRateProvider.getExRate(currency);
         // 금액 계산
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
         // 유효 시간 계산
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
-
         return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
     }
-
 }
